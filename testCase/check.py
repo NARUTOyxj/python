43683504eaa24f245560a08d333a1a85631b7fa2
yangxianjie <xianjie_yang@qq.com>
2018-01-09 07:44:45 +0800

    class 练习

--- testCase/check.py
+++ testCase/check.py
@@ -1,17 +1,17 @@
 #test 1
-def outName(s):
-	print(s)
+# def outName(s):
+# 	print(s)
 
-outName("杨贤杰")
+# outName("杨贤杰")
 
-#test 2
-def trueName(a):
-	if a <= 5:
-		print("杨杰")
-	else:
-	    print("杨贤杰")
+# #test 2
+# def trueName(a):
+# 	if a <= 5:
+# 		print("杨杰")
+# 	else:
+# 	    print("杨贤杰")
 
-trueName(10)
+# trueName(10)
 
 
 # def power(x, n):
@@ -24,23 +24,94 @@ trueName(10)
 # power(5,2)
 
 #test 3
-def girlInfo(name, gender = "female", city ="重庆" ):
+# def girlInfo(name, gender = "female", city ="重庆" ):
 
-	print("name :" , name)
-	print("gender :" , gender)
-	print("city :", city)
+# 	print("name :" , name)
+# 	print("gender :" , gender)
+# 	print("city :", city)
 
-girlInfo("杨贤杰")
-girlInfo("冉婷婷")
-girlInfo("赖靖", city = "四川")
+# girlInfo("杨贤杰")
+# girlInfo("冉婷婷")
+# girlInfo("赖靖", city = "四川")
 
 #test 4
+# class Student(object):  /类名首字母大写
+
+# 	def __init__(self, name, score):   
+# 		self.name =  name
+# 		self.score = score
+
+
+# StudentA = Student("杨贤杰", 100)
+# print(StudentA.name , StudentA.score)
+
+#test 5
+#数据封装
+# class Student(object):
+# 	"""docstring for Student"""
+# 	def __init__(self, name, score):
+# 		self.name = name
+# 		self.score = score
+
+# 	def  get_grade(self):
+# 		if self.score >= 90:
+# 			return 'A'
+# 		elif self.score >= 60:
+# 			return 'B'
+# 		else:
+# 			return 'C'
+		
+# StudentB = Student('Tom',90)
+# print(StudentB.name, StudentB.get_grade())
+
+#test 6
+#访问限制
+# class StudentC(object):
+# 	"""docstring for StudentC"""
+# 	def __init__(self, name, gender):
+# 		self.__name = name
+# 		self.__gender = gender
+
+# 	def get_name(self):
+# 		return self.__name
+
+# 	def get_gender(self):
+# 		return self.__gender
+
+# 	def set_name(self, name):
+# 		self.__name = name
+
+# 	def set_gender(self, gender):
+# 		self.__gender = gender
+		
+
+# Alice = StudentC('Alice','female')
+# Alice.set_gender('female')
+# print(Alice.get_gender())
+
+#test 7
 class Student(object):
+    count = 0
 
-	def __init__(self, name, score):
-		self.name =  name
-		self.score = score
+    def __init__(self, name):
+        self.name = name
+
+if Student.count != 0:
+    print('测试失败!1')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!2')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!3')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
+     	
 
 
-StudentA = Student("杨贤杰", 100)
-print(StudentA.name , StudentA.score)
+
+
+
