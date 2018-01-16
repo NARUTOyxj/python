0a291e72cb186ca4e59af455095d177a03369a84
yangxianjie <xianjie_yang@qq.com>
2018-01-16 22:15:07 +0800

    update

--- testCase/testLogin.py
+++ testCase/testLogin.py
@@ -1,9 +1,24 @@
 import csv
+# date = csv.reader(open('../testFile/userInfo.csv','r'))
 
-date = csv.reader(open('../testFile/userInfo.csv','r'))
+class testLogin(ConfigHttp):
+	"""docstring for testCase"""
 
-for user in date:
-	print(user)
+	def __init__(self, fileName):
+		self.fileName = fileName
+
+	def getData(self):
+		path = '../testFile/' + self.fileName
+		with open(path, newline='') as f:
+			reader = csv.reader(f)
+			for i,rows in enumerate(f):
+				if i == 1:
+					print(rows)
+
+login = testLogin('userInfo.csv')
+login.geturl()
+login.getData()
+
 
 
 
