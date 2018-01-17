e033dfb456dfefad9ade14778e52dc69855fc193
yangxianjie <xianjie_yang@qq.com>
2018-01-17 22:16:39 +0800

    update

--- testCase/testLogin.py
+++ testCase/testLogin.py
@@ -1,24 +1,32 @@
-import csv
-# date = csv.reader(open('../testFile/userInfo.csv','r'))
+import urllib.request
+import urllib.parse
+import requests  
+import json 
 
-class testLogin(ConfigHttp):
-	"""docstring for testCase"""
+# params = urllib.parse.urlencode({'username': 'plat_yxj','password':'123456'})
+# url = "http://scmbase.loongjoy.com/api/auth/postToken?%s" % params
+# with urllib.request.urlopen(url) as f:
+# 	print(f.read().decode('utf-8'))
 
-	def __init__(self, fileName):
-		self.fileName = fileName
+#python标准库
 
-	def getData(self):
-		path = '../testFile/' + self.fileName
-		with open(path, newline='') as f:
-			reader = csv.reader(f)
-			for i,rows in enumerate(f):
-				if i == 1:
-					print(rows)
+# DATA = urllib.parse.urlencode({'nickname': 'plat_yxj','password':'123456','fromSys':'scmpcapp','lang':'zh'}).encode('utf-8')
+# req = urllib.request.Request(url='http://scmbase.loongjoy.com/api/auth/postToken', data=DATA,method='POST')
+# with urllib.request.urlopen(req) as f:
+#     pass
 
-login = testLogin('userInfo.csv')
-login.geturl()
-login.getData()
+# print(f.status)
+# print(f.reason)
 
+
+
+url = 'http://scmbase.loongjoy.com/api/auth/postToken'
+data = {'nickname': 'plat06','password':'123456','fromSys':'scmpcapp','lang':'zh'}
+r = requests.post(url, data=data)  
+r = requests.post(url, data=data)  
+# 获取 接口返回的数据信息并解析（如果返回的是json格式的话）  
+json_data = json.loads(r.text)  
+print(json_data)
 
 
 
