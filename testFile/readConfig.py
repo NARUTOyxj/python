43cc23c46c5e9e83dd547cff18a71bd389765b9e
yangxianjie <xianjie_yang@loongjoy.com>
2018-02-02 18:09:34 +0800

    update readConfig.py

--- testFile/readConfig.py
+++ testFile/readConfig.py
@@ -1,6 +1,8 @@
 import os
 import codecs
 import configparser
+import sys
+sys.setrecursionlimit(1000000)
 
 proDir = os.path.split(os.path.realpath(__file__))[0]
 configPath = os.path.join(proDir, "config.ini")
@@ -9,10 +11,10 @@ class ReadConfig:
     def __init__(self):
         fd = open(configPath)
         data = fd.read()
-
+        
         #  remove BOM
-        if data[:3] == codecs.BOM_UTF8:
-            data = data[3:]
+        if data[:4] == codecs.BOM_UTF8:
+            data = data[4:]
             file = codecs.open(configPath, "w")
             file.write(data)
             file.close()
@@ -21,12 +23,22 @@ class ReadConfig:
         self.cf = configparser.ConfigParser()
         self.cf.read(configPath)
 
-    def get_email(self, name):
-        value = self.cf.get("EMAIL", name)
+    def get_section(self, title, name):
+        value = self.cf.get(title, name)
         return value
 
-    def get_http(self, name):
-        value = self.cf.get("HTTP", name)
-        return value
+    def add_section(self, title):
+        self.add_section(title)
+        
+    def set_section(self, title, name, info):
+        self.cf.set(title, name, info)
+
+test = ReadConfig()
+test.add_section('1')
+test.set_section('1','1','1')
+print(test.get_section('1','1'))
+
+
+
 
     
