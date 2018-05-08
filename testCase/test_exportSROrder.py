14c3ad861e6ebf37183063f7541a692a1de0d077
杨贤杰 <xianjie_yang@loongjoy.com>
2018-05-08 22:49:46 +0800

    sr report export is OK

--- testCase/test_exportSROrder.py
+++ testCase/test_exportSROrder.py
@@ -3,60 +3,42 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from time import sleep
 import unittest, time, re
 
-class UntitledTestCase(unittest.TestCase):
+class ExportSr(unittest.TestCase):
     def setUp(self):
+        chromeOptions = webdriver.ChromeOptions()
+        prefs = {'download.default_directory':1}
+        chromeOptions.add_experimental_option('prefs',prefs)
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
+        self.base_url = "http://scmpcapp.loongjoy.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_untitled_test_case(self):
+    def test_export_sr(self):
         driver = self.driver
-        driver.get("http://scmpcapp.loongjoy.com/#/user/login#en")
-        selenium.setSpeed("10000")
         driver.maximize_window()
-        driver.find_element_by_xpath("//input[@type='text']").click()
+        driver.get(self.base_url + "/#/user/login#zh")
+        driver.find_element_by_link_text("English").click()
         driver.find_element_by_xpath("//input[@type='text']").clear()
         driver.find_element_by_xpath("//input[@type='text']").send_keys("plat_yxj")
-        driver.find_element_by_xpath("//body[@id='body']/div/div/div[2]/div[2]").click()
-        driver.find_element_by_xpath("//input[@type='password']").click()
-        driver.find_element_by_xpath("//input[@type='password']").clear()
         driver.find_element_by_xpath("//input[@type='password']").send_keys("123456")
-        driver.find_element_by_xpath("//button[@type='submit']").click()
-        driver.implicitly_wait(30)
-        driver.find_element_by_link_text("Finance").click()
-        driver.implicitly_wait(30)
+        driver.find_element_by_css_selector("button.submit.ng-scope").click()
+        driver.find_element_by_css_selector("i.icon-finance").click()
         driver.find_element_by_xpath("(//a[contains(text(),'SR Report')])[2]").click()
-        driver.find_element_by_xpath("//body[@id='body']/div/div/div/div/div/div/div/div[2]/form/div[3]/div/select").click()
-        driver.implicitly_wait(30)
         Select(driver.find_element_by_xpath("//body[@id='body']/div/div/div/div/div/div/div/div[2]/form/div[3]/div/select")).select_by_visible_text("Xiaomi")
-        driver.implicitly_wait(30)
-        driver.find_element_by_xpath("//option[@value='3']").click()
-        driver.find_element_by_xpath("//body[@id='body']/div/div/div/div/div/div/div/div[2]/form/div[3]/div[2]/select").click()
-        driver.implicitly_wait(30)
         Select(driver.find_element_by_xpath("//body[@id='body']/div/div/div/div/div/div/div/div[2]/form/div[3]/div[2]/select")).select_by_visible_text("Phone")
-        driver.implicitly_wait(30)
-        driver.find_element_by_xpath("//option[@value='phone']").click()
-        driver.find_element_by_xpath("//body[@id='body']/div/div/div/div/div/div/div/div[2]/form/div[3]/div[5]/select").click()
-        driver.implicitly_wait(30)
-        Select(driver.find_element_by_xpath("//body[@id='body']/div/div/div/div/div/div/div/div[2]/form/div[3]/div[5]/select")).select_by_visible_text("Create Time")
-        driver.implicitly_wait(30)
-        driver.find_element_by_xpath("(//option[@value='1'])[3]").click()
-        driver.implicitly_wait(30)
-        driver.find_element_by_xpath("//input[@type='text']").click()
-        driver.implicitly_wait(30)
         driver.find_element_by_xpath("//button[@type='submit']").click()
-        driver.implicitly_wait(50)
+        sleep(5)
         driver.find_element_by_link_text("Export Report").click()
-        # sel.selectWindow(sel.getAllWindowTitles()[1])
-        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
+        sleep(30)
         driver.close()
-        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_local | ]]
+        # ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp |  | 30000]]
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
